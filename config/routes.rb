bf60d88b55cd73961f17c0240605dd781e101024
filmayui <zhongchuyangjie786@gmail.com>
2020-11-23 04:36:41 +0900

    ユーザーマイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root to: "artworks#index"
+
+  resources :users, only: [:show] do
+    member do
+      get 'favorite'
+    end
+  end
   
 end
