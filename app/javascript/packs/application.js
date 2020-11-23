229d27db0e2169898fff877938ca70f2643ead66
filmayui <zhongchuyangjie786@gmail.com>
2020-11-23 10:20:25 +0900

    クレジットカード機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 //require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../calculate_donation")
+require("../card")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_202600) do
+ActiveRecord::Schema.define(version: 2020_11_23_010942) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,15 @@ ActiveRecord::Schema.define(version: 2020_11_22_202600) do
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
   end
 
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "card_token", null: false
+    t.string "customer_token", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "text", null: false
     t.bigint "user_id", null: false
@@ -106,6 +115,7 @@ ActiveRecord::Schema.define(version: 2020_11_22_202600) do
   add_foreign_key "artists", "donation_projects"
   add_foreign_key "artists", "users"
   add_foreign_key "artworks", "artists"
+  add_foreign_key "cards", "users"
   add_foreign_key "comments", "artworks"
   add_foreign_key "comments", "users"
 end
